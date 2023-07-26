45333fb3cd83285d7d1351b26e23656836e365b0
Paula Vergara <paulajve@gmail.com>
2023-07-26 11:18:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup
 
+    setup(
+        name='factorial',
+        version='1.0.0',
+        author='Bard',
+        author_email='bard@example.com',
+        description='A Python package for calculating factorials.',
+        url='https://github.com/bard/factorial',
+        packages=['factorial'],
+        install_requires=[],
+        entry_points={
+            'console_scripts': ['factorial=factorial.factorial:main'],
+        },
+    )
